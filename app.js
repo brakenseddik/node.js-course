25997cac4cc2cf7f31368e9dc4cf8731e29e406b
unknown <dzmidou95@gmail.com>
2021-02-17 21:48:44 +0100

    mongo db mongoose

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
 const express =require('express');
+const morgan =require('morgan');
+const mongoose = require('mongoose');
+const Blog=require('./models/blog')
 const app=express();
 
+const dburl='mongodb+srv://seddik:sadikdz1998@cluster0.a8v8s.mongodb.net/bloggey?retryWrites=true&w=majority';
+
+mongoose.connect(dburl,{useNewUrlParser: true, useUnifiedTopology: true})
+.then((result)=>{
+    app.listen(3000);
+}).catch((error)=>{
+    console.log(error);
+
+});
 //LISTEN FOR REQUESTS
-app.listen(3000);
+
 
 //REGISTER VIEW ENGINE
 
 app.set('view engine','ejs');
+
+// PUBLIC STATIC FILES
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+
+app.get('/add-blog',(req,res,next)=>{
+const blog=new Blog({
+    title:'new blog 2',
+    snippet:'about my blog 2',
+    body:'more about my blog 2'
+});
+
+blog.save().then((result)=>{res.send(result)}).catch((err)=>{console.log(err)});
+});
+
+app.get('/all-blogs',
+(req,res,next)=>{
+  Blog.find().then((result)=>{res.send(result)}).catch((err)=>{console.log(err)});
+    });
+
+app.get('/single-blog', (req, res) => {
+        Blog.findById('602d7d68a863b52664111d1e')
+          .then(result => {
+            res.send(result);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+
+// app.use((req,res,next)=>{
+// console.log('middleware 1');
+// next();
+// })
 
 //ROUTING
 app.get('/',(req,res)=>{
